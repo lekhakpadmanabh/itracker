bcbf58cdc74bc412f308b05707c8b068b3dba163
Padmanabh <prabhas.padmanabh@gmail.com>
2014-06-06 01:46:21 +0530

    Added a form to add issues

--- issuetracker_proj/issuetracker/views.py
+++ issuetracker_proj/issuetracker/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 from .models import Issue
+from .forms import IssueForm
 """ FBV to be changed """
 
 def index(request):
-    issues = Issue.objects.all()
-    return render(request,
-        'issuetracker/index.html',
-        {'issues': issues},
-        )
+    if request.method == 'POST':
+        form = IssueForm(data = request.POST)
+        if form.is_valid():
+            new_issue = form.save()
+            return HttpResponseRedirect(reverse(
+                'issue_detail',
+                args = (new_issue.pk,)))
+
+    else:
+        issues = Issue.objects.all()
+        form = IssueForm()
+        return render(request,
+            'issuetracker/index.html',
+            {'issues': issues,
+             'form': form},
+            )
 
 def detail(request,pk):
     issue = get_object_or_404(Issue, pk=pk)
